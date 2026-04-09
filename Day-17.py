4536a31b79cd7b7f2c5a0e7886ebf5ee16865e92
kartheek05 <kumarkartheek2005@gmail.com>
2026-04-09 13:03:51 +0530

    Lisit Comprehension

---
+++ Day-17.py
@@ -0,0 +1,24 @@
+'''
+Lambda function()-------->
+-->This is a also called as a Anonymous fuction.
+-->A lambda function can take n number of arguments but have only one expression.
+Syntax-->
+lambda(keyword)arguments : expression
+Examples:
+
+#Example_1
+any = lambda so , do : so + do
+print(any(6,10))
+print(any(int(input("Enter")),int(input("enter"))))
+
+#Example_2
+more = lambda kar , kum , theek : (kar + theek)*kum
+print(more(int(input("enter")),int(input("enter")),int(input("enter"))))
+
+List Comprehension:
+--->This offers the shorter syntax when you want to create a new list form the exisiting list
+Syntax ----> variable_name = [enpression loop and condition]
+'''
+old_list = [1,2,3,4,5,6]
+new_list = [j for j in old_list if j % 2 == 0]
+print(new_list)
