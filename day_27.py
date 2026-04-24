be25e353fdaaf5798f3b45c137b038de481033a4
kartheek05 <kumarkartheek2005@gmail.com>
2026-04-24 23:23:07 +0530

    day_27

---
+++ day_27.py
@@ -0,0 +1,52 @@
+'''
+Handling Errors:
+1.Try BLock
+2.Except Block
+'''
+'''
+try block: the try block will test a block of code for errors
+Except Block: This Block will Take care of my errors
+Else Block: Else keyword to define a block of code to be excuted if no error were raised.
+Finally Block: This block wil execute either try block having any error or no error.
+'''
+# try:
+#     print(b)
+# except:
+#     print("This block can handle error")
+
+# try:
+#     print(b)
+# except NameError:
+#     print("B is not defined")
+
+# try:
+#     a = 9
+#     b = 8
+#     print(a+b)
+# except:
+#     print("Error Here")
+# else:
+#     print("These is no error")
+
+# try:
+#     a = 9
+#     b = 8
+#     print(a//b)
+# except:
+#     print("Error Here")
+# else:
+#     print("These is no error")
+
+# try:
+#     num = int(input("Enter the number: "))
+#     num2 = int(input("Enter the number: "))
+#     result = num/ num2
+# except ValueError:
+#     print("pls enter a valid number")
+# except ZeroDivisionError:
+#     print("Cannot didvide by zero")
+# else:
+#     print(f"result = {result}")
+# finally:
+#     print("Program is completed")
+
